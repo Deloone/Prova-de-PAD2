c714defcc51ac21712c93dd6893cb6a32a9e4f31
Arthur <78800537+Deloone@users.noreply.github.com>
2021-06-17 15:21:11 -0300

    segundo

--- provinha.js
+++ provinha.js
@@ -6,3 +6,17 @@ function someFun(params, callback) {
   return 1
 }
 
+//2º 
+
+function someFn(num) {
+  const returnFunction = (value) => {
+    switch (value) {
+      case 3:
+        return 4
+      case 1:
+        return 5
+      case 5:
+        return 10
+    }
+  }
+}
